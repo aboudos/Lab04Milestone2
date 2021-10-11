e260a70c576f3040206ff481a31ff22c6ec2da39
aboudos <aboudos@wisc.edu>
2021-10-11 14:42:54 -0500

    Initial commit

---
+++ app/src/main/java/com/example/lab04milestone2/MainActivity.java
@@ -0,0 +1,138 @@
+package com.example.lab04milestone2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    LocationManager locationManager;
+    LocationListener locationListener;
+    TextView latitude;
+    TextView longitude;
+    TextView accuracy;
+    TextView altitude;
+    TextView address;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                Log.i("LocationInfo", "Ran from OnlocationChanged()");
+                updateLocationInfo(location);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(@NonNull String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(@NonNull String s) {
+
+            }
+        };
+
+        if(Build.VERSION.SDK_INT <23){
+            startListening();
+        } else {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            } else {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,0, locationListener);
+                Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if(location != null) {
+                    updateLocationInfo(location);
+                    Log.i("LocationInfo", "Ran from onCreate()");
+                }
+            }
+        }
+    }
+
+    public void startListening(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            startListening();
+        }
+    }
+
+    public void updateLocationInfo(Location location){
+        Log.i("LocationInfo", location.toString());
+        latitude = findViewById(R.id.Latitude);
+        longitude = findViewById(R.id.Longitude);
+        accuracy = findViewById(R.id.Accuracy);
+        altitude = findViewById(R.id.Altitude);
+        latitude.setText("Latitude: " + location.getLatitude());
+        longitude.setText("Longitude: " + location.getLongitude());
+        altitude.setText("Altitude: " + location.getAltitude());
+        accuracy.setText("Accuracy: " + location.getAccuracy());
+
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+        try {
+            String addressString = "Could not find address";
+            List<Address> listAddresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+            if (listAddresses != null && listAddresses.size() >0){
+                Log.i("PlaceInfo", listAddresses.get(0).toString());
+                addressString = "Address: \n";
+                if(listAddresses.get(0).getSubThoroughfare() != null){
+                    addressString += listAddresses.get(0).getSubThoroughfare() + " ";
+                }
+                if (listAddresses.get(0).getThoroughfare() != null){
+                    addressString += listAddresses.get(0).getThoroughfare() + "\n";
+                }
+                if (listAddresses.get(0).getLocality() != null){
+                    addressString += listAddresses.get(0).getLocality() + "\n";
+                }
+                if (listAddresses.get(0).getPostalCode() != null){
+                    addressString += listAddresses.get(0).getPostalCode() + "\n";
+                }
+                if (listAddresses.get(0).getCountryName() != null){
+                    addressString += listAddresses.get(0).getCountryName() + "\n";
+                }
+            }
+
+            TextView addy = (TextView) findViewById(R.id.Address);
+            addy.setText(addressString);
+
+        } catch(IOException e){
+            Log.i("LocationInfo", "Exception in Geocorder.");
+            e.printStackTrace();
+        }
+    }
+}
